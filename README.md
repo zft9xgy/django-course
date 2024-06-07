663db4726cfaadcfac0ea8402d0e36959abc8e90
zft9xgy <zft9xgy@icloud.com>
2024-06-07 14:35:00 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # django-course
+
+Este proyecto se base en el curso de (Dennis Ivvy)[https://dennisivy.com/]: (Django 2021)[https://github.com/divanov11/Django-2021/blob/master/projects/templates/projects/project_form.html]
---
+++ projects/views.py
@@ -0,0 +1,69 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Project
+from .forms import ProjectForm
+
+# Create your views here.
+# Aqui controlamos toda la lógica del programa. 
+# En la respuesta le mandamos el template con el contexto.
+
+
+def projects(request):
+    projects = Project.objects.all()
+    
+    context = {
+        'projects': projects
+    }
+    return render(request, 'projects/projects.html',context)
+
+
+def project(request,pk):
+    projectObj = Project.objects.get(id=pk)
+    context = {
+        'project': projectObj
+    }
+    return render(request, 'projects/single-project.html',context)
+
+
+def createProject(request):
+    form = ProjectForm()
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
+
+    context = {
+        'form': form
+    }
+    return render(request, 'projects/project-form.html',context)
+
+
+def updateProject(request,pk):
+    projectObj = Project.objects.get(id=pk)
+    form = ProjectForm(instance=projectObj)
+
+    if request.method == 'POST':
+        form = ProjectForm(request.POST,instance=projectObj)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+
+
+    context = {
+        'form': form
+    }
+    return render(request, 'projects/project-form.html',context)
+
+def deleteProject(request,pk):
+    projectObj = Project.objects.get(id=pk)
+    if request.method == 'POST':
+        projectObj.delete()
+        return redirect('projects')
+        
+    context = {
+        'object': projectObj
+    }
+    return render(request, 'projects/delete-object.html',context)
