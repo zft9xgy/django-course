b591560f6bbf0dbbfaf0b07037c7a8380f561542
zft9xgy <zft9xgy@icloud.com>
2024-06-10 16:20:27 +0200

    Add and render profiles

--- users/models.py
+++ users/models.py
@@ -33,3 +33,14 @@ class Profile(models.Model):
     
 
 
+class Skill(models.Model):
+    owner = models.ForeignKey(Profile,on_delete=models.CASCADE,null=True,blank=True)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+
+    def __str__(self):
+        return str(self.name)
--- users/views.py
+++ users/views.py
@@ -9,3 +9,13 @@ def profiles(request):
 
     context = {'profiles':profiles}
     return render(request,'users/profiles.html',context)
+
+def userProfile(request,pk):
+    profileObj = Profile.objects.get(id=pk)
+
+    topSkills = profileObj.skill_set.exclude(description__exact="")
+    otherSkills = profileObj.skill_set.filter(description="")
+
+    context = {'profile':profileObj,'topSkills':topSkills,'otherSkills':otherSkills}
+    return render(request,'users/user-profile.html',context)
+
