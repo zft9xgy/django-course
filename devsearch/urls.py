95b7568600bcccea001799ed2e24457ffd9eb6d1
zft9xgy <zft9xgy@icloud.com>
2024-06-10 15:07:58 +0200

    User app. Profiles models and render.

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -22,6 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('projects.urls')),
+    path('profiles/',include('users.urls')),
+
 ] 
 
 
